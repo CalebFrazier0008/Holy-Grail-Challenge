558d123819968ed02b96e4f0b7c3bfd0e8dc43dc
CalebFrazier0008 <157289715+CalebFrazier0008@users.noreply.github.com>
2024-02-14 06:44:25 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function outerGrid() {
+  const outerGridElement = document.querySelector(".Outer-grid");
+  outerGridElement.classList.toggle("Outer-grid-expanded");
+
+  const outerGridElement2 = document.querySelector("nav");
+  outerGridElement2.classList.toggle("Hide-nav");
+}
